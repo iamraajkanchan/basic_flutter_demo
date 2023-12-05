724b3d3331077d42df9d23c65f3a1203521e81e2
Raj Kanchan <iamraajkanchan@gmail.com>
2023-12-05 16:16:06 +0530

    Album details from internet is added in the project. Modifications made in main.dart, DebugProfile.entitlements, pubspec.lock and pubspec.yaml. Also album.dart and album_fetcher.dart is added.

---
+++ lib/views/album_fetcher.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../model/album.dart';
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class AlbumFetcher extends StatefulWidget {
+  const AlbumFetcher({super.key, required this.pageTitle});
+
+  final String pageTitle;
+
+  @override
+  State<AlbumFetcher> createState() => _AlbumFetcher();
+}
+
+class _AlbumFetcher extends State<AlbumFetcher> {
+  late Future<Album> albumList;
+
+  @override
+  void initState() {
+    super.initState();
+    albumList = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.pageTitle),
+      ),
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: albumList,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+            } else {
+              return Text("${snapshot.error}");
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
