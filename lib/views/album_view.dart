5836c732d40d32a8431358446aa3bf43b9c88d27
Raj Kanchan <iamraajkanchan@gmail.com>
2023-12-07 09:21:47 +0530

    Custom album_view.dart is updated.

--- lib/views/album_view.dart
+++ lib/views/album_view.dart
@@ -10,23 +10,31 @@ class AlbumView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Expanded(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-              color: const Color.fromRGBO(124, 124, 124, 95.0),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Text("${album.id}"),
-              )),
-          Text(
-            album.title,
-            softWrap: true,
-            maxLines: 2,
-          )
-        ],
-      ),
+        child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        DecoratedBox(
+          decoration: const BoxDecoration(
+            color: Colors.lime,
+            shape: BoxShape.rectangle
+          ),
+          child: Text(
+            "${album.id}",
+            style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        const Spacer(flex: 1,),
+        Expanded(
+            flex: 4,
+            child: Text(
+              album.title,
+              softWrap: true,
+              maxLines: 2,
+            ))
+      ],
     ));
   }
 }
